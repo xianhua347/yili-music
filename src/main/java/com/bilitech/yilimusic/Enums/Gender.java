800f3ccf97bb05078e270febfae0519251fe68b4
xianhua347 <3389162133@qq.com>
2022-10-23 11:44:51 +0800

    feat🤩(Enums):add Gender

---
+++ src/main/java/com/bilitech/yilimusic/Enums/Gender.java
@@ -0,0 +1,7 @@
+package com.bilitech.yilimusic.Enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    UNKNOWN,
+}
---
+++ src/main/java/com/bilitech/yilimusic/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bilitech.yilimusic.Repository;
+
+import com.bilitech.yilimusic.enetity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+
+public interface UserRepository extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
+}
