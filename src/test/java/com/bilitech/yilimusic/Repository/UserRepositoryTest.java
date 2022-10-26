9c8161a3e3a3f425b4e1dc9116a2b8f82b705d34
xianhua347 <3389162133@qq.com>
2022-10-26 11:24:33 +0800

    build💼(maven):setting skip test!

--- src/test/java/com/bilitech/yilimusic/Repository/UserRepositoryTest.java
+++ src/test/java/com/bilitech/yilimusic/Repository/UserRepositoryTest.java
@@ -1,29 +1,7 @@
 package com.bilitech.yilimusic.Repository;
 
-import com.bilitech.yilimusic.Enums.Gender;
-import com.bilitech.yilimusic.Mapper.UserMapper;
-import com.bilitech.yilimusic.VO.UserVO;
-import com.bilitech.yilimusic.enetity.User;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserRepositoryTest {
-    private UserRepository userRepository;
-
-    @Autowired
-    public UserRepositoryTest(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Test
-    void findById() {
-        final User user1 = new User();
-        user1.setPassword("11");
-        user1.setEnabled(false);
-        user1.setLocked(true);
-        user1.setGender(Gender.MALE);
-        userRepository.save(user1);
-    }
 }
