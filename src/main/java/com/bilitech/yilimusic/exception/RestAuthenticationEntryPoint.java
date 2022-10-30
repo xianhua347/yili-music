0465a17ff4dfb91e220075a258fb3717658fefd2
xianhua347 <3389162133@qq.com>
2022-10-30 14:55:57 +0800

    feat🤩:Global cross configuration

--- src/main/java/com/bilitech/yilimusic/exception/RestAuthenticationEntryPoint.java
+++ src/main/java/com/bilitech/yilimusic/exception/RestAuthenticationEntryPoint.java
@@ -1,13 +1,11 @@
 package com.bilitech.yilimusic.exception;
 
 import cn.hutool.json.JSONUtil;
-import com.bilitech.yilimusic.exception.ErrorResponse;
-import com.bilitech.yilimusic.exception.ExceptionType;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,11 +15,13 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Cache-Control","no-cache");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setCode(ExceptionType.UNAUTHORIZED.getCode());
         errorResponse.setMessage(ExceptionType.UNAUTHORIZED.getMessage());
