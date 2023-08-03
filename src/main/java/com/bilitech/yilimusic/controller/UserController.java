53d43f9d22ae302d7d7fbc2c298b370e5bef32d5
chenxiaohua <3389162133@qq.com>
2023-08-03 16:51:02 +0800

    update:删除sout语句

--- src/main/java/com/bilitech/yilimusic/controller/UserController.java
+++ src/main/java/com/bilitech/yilimusic/controller/UserController.java
@@ -35,7 +35,6 @@ public class UserController {
 
   @GetMapping("{name}")
   public ApiResponse<UserVO> get(@PathVariable String name) {
-    System.out.println(userService.getUser(name).toString());
     return ApiResponse.success(userMapper.toVo(userService.getUser(name)));
   }
 
