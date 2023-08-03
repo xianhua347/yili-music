4b3aa7770e6ed7cb2e51a6102e6ed44d4944e8ed
chenxiaohua <3389162133@qq.com>
2023-08-03 15:57:03 +0800

    feat:修复UserDTO字段

--- src/main/java/com/bilitech/yilimusic/Mapper/Dto/UserCreateDto.java
+++ src/main/java/com/bilitech/yilimusic/Mapper/Dto/UserCreateDto.java
@@ -1,11 +1,17 @@
 package com.bilitech.yilimusic.Mapper.Dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserCreateDto {
 
+  @NotBlank(message = "用户名不能为空")
+  @Size(min = 4, max = 64, message = "用户名长度应该在4个字符到64个字符之间")
   private String username;
 
+  @NotBlank(message = "用户名不能为空")
+  @Size(min = 6, max = 64, message = "密码长度应该在4个字符到64个字符之间")
   private String password;
 }
--- src/main/java/com/bilitech/yilimusic/Mapper/Dto/UserDto.java
+++ src/main/java/com/bilitech/yilimusic/Mapper/Dto/UserDto.java
@@ -1,11 +1,12 @@
 package com.bilitech.yilimusic.Mapper.Dto;
 
+import com.bilitech.yilimusic.Enums.Gender;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class UserDto {
-
   private String id;
 
   private String username;
@@ -13,4 +14,14 @@ public class UserDto {
   private String nickname;
 
   private List<RoleDto> roles;
+
+  private Gender gender;
+
+  private Boolean locked;
+
+  private Boolean enabled;
+
+  private String lastLoginIp;
+
+  private LocalDateTime lastLoginTime;
 }
