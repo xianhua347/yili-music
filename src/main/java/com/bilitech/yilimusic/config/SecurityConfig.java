5721d7af356b45ec421a8e8ae7f2183b8e52e28a
chenxiaohua <3389162133@qq.com>
2023-08-14 16:07:28 +0800

    update: 删除ApiResponse错误返回体

--- src/main/java/com/bilitech/yilimusic/config/SecurityConfig.java
+++ src/main/java/com/bilitech/yilimusic/config/SecurityConfig.java
@@ -1,18 +1,20 @@
 package com.bilitech.yilimusic.config;
 
-// import com.bilitech.yilimusic.Filter.JWTAuthenticationFilter;
 
 import com.bilitech.yilimusic.Filter.JWTAuthenticationFilter;
 import com.bilitech.yilimusic.Filter.JWTAuthorizationFilter;
 import com.bilitech.yilimusic.Service.UserService;
 import com.bilitech.yilimusic.exception.RestAuthenticationEntryPoint;
 import javax.annotation.Resource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * @author 陈现府
@@ -27,18 +29,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.POST, AuthenticationConfigConstants.SIGN_UP_URL).permitAll()
-        .antMatchers("/users/login", "/users/register").permitAll()
-        .antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-        .anyRequest().authenticated()
-        .and()
+    http
+        .cors(AbstractHttpConfigurer :: disable)
+        .csrf(AbstractHttpConfigurer :: disable)
+        .authorizeRequests(authorize -> authorize
+            .antMatchers(HttpMethod.POST, AuthenticationConfigConstants.SIGN_UP_URL).permitAll()
+            .antMatchers("/users/login", "/users/register").permitAll()
+            .antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+            .anyRequest().authenticated()
+        )
         .addFilter(new JWTAuthenticationFilter(authenticationManager()))
         .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-        .exceptionHandling() //添加exception处理 如果处理的话 所有请求和错误不会有返回结果
-        .authenticationEntryPoint(restAuthenticationEntryPoint)//接管认证的异常处理
-        .and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .exceptionHandling(exceptionHandling -> exceptionHandling  // 添加exception处理，如果处理的话，所有请求和错误不会有返回结果
+            .authenticationEntryPoint(restAuthenticationEntryPoint) // 接管认证的异常处理
+        )
+        .sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
   }
 
   @Override
