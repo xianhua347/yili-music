9ba80892761a634c0107859149e1448126846674
chenxiaohua <3389162133@qq.com>
2023-09-29 00:08:22 +0800

    feat:Add Lombok annotation
    @AllArgsConstructor
    @NoArgsConstructor

--- src/main/java/com/bilitech/yilimusic/utils/ApiResponse.java
+++ src/main/java/com/bilitech/yilimusic/utils/ApiResponse.java
@@ -1,20 +1,20 @@
 package com.bilitech.yilimusic.utils;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.io.Serializable;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@ApiResponses
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> implements Serializable {
 
-  private Integer code;
+  private  Integer code;
   private String message;
   private T data;
-
-  private ApiResponse(Integer code, String message, T data) {
-    this.code = code;
-    this.message = message;
-    this.data = data;
-  }
 
   public static <T> ApiResponse<T> success(T data) {
     return new ApiResponse<>(200, "Success", data);
