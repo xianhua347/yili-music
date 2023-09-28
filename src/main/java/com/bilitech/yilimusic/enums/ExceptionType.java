7ada581ea958c9cf1b0e4107d5b427c44f9e0bae
chenxiaohua <3389162133@qq.com>
2023-09-29 00:10:43 +0800

    feat:Added some business exceptions.

--- src/main/java/com/bilitech/yilimusic/enums/ExceptionType.java
+++ src/main/java/com/bilitech/yilimusic/enums/ExceptionType.java
@@ -21,6 +21,7 @@ public enum ExceptionType {
   NOT_FOUND(404, "未找到"),
   USER_DELETE_FAILED(4000105, "用户删除失败"),
   USER_UPDATE_FAILED(4000106, "用户更新失败"),
+  USER_NOT_EXIST(4000106, "用户不存在"),
 
   USER_NAME_DUPLICATE(4000101, "用户名重复"),
   USERNAME_ALREADY_EXIST(4000101, "用户已存在"),
@@ -32,7 +33,13 @@ public enum ExceptionType {
   USER_PASSWORD_NOT_MATCH(4000102, "用户名或密码错误"),
 
   USER_DISABLED(4000103, "用户已被禁用"),
-  USER_ACCOUNT_LOCKED(4000104, "用户已被锁定");
+  USER_ACCOUNT_LOCKED(4000104, "用户已被锁定"),
+  ROLE_NOT_FOUND(4000105, "没有找到当前角色"),
+
+  // JWT相关异常
+  INVALID_TOKEN(4000106, "非法token"),
+  TOKEN_EXPIRED(4000107, "token已过期"),
+  REFRESH_TOKEN_EXPIRED(4000108,"刷新令牌已过期");
 
   private final Integer code;
 
