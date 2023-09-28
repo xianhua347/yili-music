8f5f047caf38c1da6c09b26e57061a24913920c2
chenxiaohua <3389162133@qq.com>
2023-09-28 23:59:29 +0800

    feat:Add support for Spring asynchronous processing and JetCache.

--- src/main/java/com/bilitech/yilimusic/YiliMusicApplication.java
+++ src/main/java/com/bilitech/yilimusic/YiliMusicApplication.java
@@ -1,19 +1,24 @@
 package com.bilitech.yilimusic;
 
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
+@EnableMethodCache(basePackages = "com.bilitech.yilimusic")
 public class YiliMusicApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(YiliMusicApplication.class, args);
   }
+
   @Bean
   public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
     return new JPAQueryFactory(entityManager);
