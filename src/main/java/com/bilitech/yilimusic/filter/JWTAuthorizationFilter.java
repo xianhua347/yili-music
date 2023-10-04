5b3cfbfea5117a06f2e5b31d41e8e6de4a067149
chenxiaohua <3389162133@qq.com>
2023-10-04 13:56:06 +0800

    refactor(JWTAuthorizationFilter): Refactor JWT authorization filter
    Refactored JWT authorization filter JWTAuthorizationFilter, optimizing code structure and performance.

--- src/main/java/com/bilitech/yilimusic/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/bilitech/yilimusic/filter/JWTAuthorizationFilter.java
@@ -1,72 +1,87 @@
 package com.bilitech.yilimusic.filter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.bilitech.yilimusic.config.AuthenticationConfigConstants;
-import com.bilitech.yilimusic.enetity.User;
+import static com.bilitech.yilimusic.utils.ResponseUtil.sendErrorResponse;
+
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.bilitech.yilimusic.config.JwtSecurityProperties;
+import com.bilitech.yilimusic.enums.ExceptionType;
+import com.bilitech.yilimusic.model.enetity.User;
 import com.bilitech.yilimusic.service.UserService;
+import com.bilitech.yilimusic.utils.JwtUtils;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AuthenticationManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 
 /**
  * 用户权限 JWT token
  */
-public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
   private final UserService userService;
 
-  public JWTAuthorizationFilter(AuthenticationManager authenticationManager,
-      UserService userService) {
-    super(authenticationManager);
-    this.userService = userService;
-  }
+  private final JwtSecurityProperties jwtSecurityProperties;
+
+  private final JwtUtils jwtUtils;
 
   /**
    * 过滤器
-   * @param request 请求
+   *
+   * @param request  请求
    * @param response 响应
-   * @param chain 过滤器链
-   * @throws IOException IO异常
+   * @param chain    过滤器链
+   * @throws IOException      IO异常
    * @throws ServletException Servlet异常
    */
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
-    //获取到token
-    final String header = request.getHeader(AuthenticationConfigConstants.HEADER_STRING);
+  protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response,
+      @NotNull FilterChain chain) throws IOException, ServletException {
+    //拿到Authorization字段对应的信息
+    final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
     //判断是否有token 如果没有token直接doFilter也就是放行 说明用户根本没有注册
-    if (header == null || ! header.startsWith(AuthenticationConfigConstants.TOKEN_PREFIX)) {
+    if (authorization == null || ! authorization.startsWith(
+        jwtSecurityProperties.getTokenStartWith())) {
       chain.doFilter(request, response);
       return;
     }
-    //拿到 header里面的token做验证
-    UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(header);
-    //把token塞到Spring mvc上下文中
-    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-    chain.doFilter(request, response);
+    try {
+      //拿到 header里面的token做验证
+      UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(authorization);
+      //把token塞到Spring Security上下文中
+      SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      chain.doFilter(request, response);
+    } catch (TokenExpiredException e) {
+      log.warn("token 已过期 " + e);
+      sendErrorResponse(response, HttpStatus.OK, ExceptionType.TOKEN_EXPIRED);
+    }
   }
 
   /**
-   * 获取用户权限的token
-   * @param header 请求头
+   * 验证前端传来的jwt token并且返回username+role信息构成的UsernamePasswordAuthenticationToken
+   *
+   * @param authorizationHeader 请求头
    * @return UsernamePasswordAuthenticationToken
    */
-  private UsernamePasswordAuthenticationToken getAuthentication(String header) {
+  private UsernamePasswordAuthenticationToken getAuthentication(String authorizationHeader) {
     //如果有token就进行解析
-    if (header != null) {
-      String username = JWT.require(
-              Algorithm.HMAC512(AuthenticationConfigConstants.SECRET.getBytes()))
-          .build()
-          .verify(header.replace(AuthenticationConfigConstants.TOKEN_PREFIX, ""))
-          .getSubject();
+    if (authorizationHeader != null) {
+      String username = jwtUtils.getUserNameFromJwtToken(authorizationHeader);
       //判断用户名是否为空 如果不为空就返回一个用户权限的token
       if (username != null) {
         User user = userService.loadUserByUsername(username);
