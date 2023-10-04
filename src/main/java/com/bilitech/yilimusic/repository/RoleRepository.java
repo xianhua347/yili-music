f21ac488f52264aa8f211688f3622098f2316aa0
chenxiaohua <3389162133@qq.com>
2023-10-04 14:30:20 +0800

    refactor🛠️(RoleRepository): Update RoleRepository class

--- src/main/java/com/bilitech/yilimusic/repository/RoleRepository.java
+++ src/main/java/com/bilitech/yilimusic/repository/RoleRepository.java
@@ -1,10 +1,14 @@
 package com.bilitech.yilimusic.repository;
 
-import com.bilitech.yilimusic.enetity.Role;
+import com.bilitech.yilimusic.model.enetity.Role;
+import io.vavr.control.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, String>, QuerydslPredicateExecutor<Role> {
+public interface RoleRepository extends JpaRepository<Role, String>,
+    QuerydslPredicateExecutor<Role> {
+
+  Option<Role> findByName(String name);
 }
